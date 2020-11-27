c6ada153a7ed7a0916ff8f2dce302fe9bb5a1654
mens-rea <juan.miguel.g.alvarez@gmail.com>
2020-11-28 00:53:13 +0800

    get specific features on a merged table

--- run_analysis.R
+++ run_analysis.R
@@ -5,20 +5,46 @@ setwd("~/Desktop/getting_and_cleaning_data/")
 curr <- getwd()
 print(curr)
 
-# get mean of every measurement
-getMeanList <- function(list) {
+# get specific values from the given features on all axis
+getDataFeatures <- function(list) {
+  features <- data.frame()
   for (i in 1:length(list)){
     items <- strsplit(list[i], " ")
     unlisted <- unlist(items)
-    asVector <- as.vector(as.numeric(unlisted)) ;
-    result <- mean(asVector, na.rm=TRUE)
-    sd <- sd(asVector, na.rm=TRUE)
-    list[i] <- result
+    asVector <- as.vector(as.numeric(unlisted))
+    
+    # getting tbody acceleration mean
+    v1 <- asVector[1]
+    v2 <- asVector[2]
+    v3 <- asVector[3]
+    # getting tbody acceleration standard deviation
+    v4 <- asVector[4]
+    v5 <- asVector[5]
+    v6 <- asVector[6]
+    # getting tbody gyro mean
+    v7 <- asVector[121]
+    v8 <- asVector[122]
+    v9 <- asVector[123]
+    # getting tbody gyro standard deviation
+    v10 <- asVector[124]
+    v11 <- asVector[125]
+    v12 <- asVector[126]
+    # getting fbody acceleration mean
+    v13 <- asVector[266]
+    v14 <- asVector[267]
+    v15 <- asVector[268]
+    # getting fbody acceleration standard deviation
+    v16 <- asVector[269]
+    v17 <- asVector[270]
+    v18 <- asVector[271]
+    
+    newRow <- data.frame(v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16,v17,v18)
+    features <- rbind(features, newRow)
   }
-  return(list)
+  return(features)
 }
 
-# replace activity values with proper labels
+# mapping activity values to proper labels
 replaceActivityLabels <- function(output) {
   output$activity[output$activity==1] <- "WALKING"
   output$activity[output$activity==2] <- "WALKING_UPSTAIRS"
@@ -29,80 +55,28 @@ replaceActivityLabels <- function(output) {
   return(output)
 }
 
-# create training data
+# read training data
 train_subject <- readLines("dataset/train/subject_train.txt")
 xtrain <- readLines("dataset/train/X_train.txt")
 ytrain <- readLines("dataset/train/y_train.txt")
 
-train_body_acc_x <- readLines("dataset/train/Inertial Signals/body_acc_x_train.txt")
-train_body_acc_y <- readLines("dataset/train/Inertial Signals/body_acc_y_train.txt")
-train_body_acc_z <- readLines("dataset/train/Inertial Signals/body_acc_z_train.txt")
-
-train_body_gyro_x <- readLines("dataset/train/Inertial Signals/body_gyro_x_train.txt")
-train_body_gyro_y <- readLines("dataset/train/Inertial Signals/body_gyro_y_train.txt")
-train_body_gyro_z <- readLines("dataset/train/Inertial Signals/body_gyro_z_train.txt")
-
-train_total_acc_x <- readLines("dataset/train/Inertial Signals/total_acc_x_train.txt")
-train_total_acc_y <- readLines("dataset/train/Inertial Signals/total_acc_y_train.txt")
-train_total_acc_z <- readLines("dataset/train/Inertial Signals/total_acc_z_train.txt")
-
-trbax <- getMeanList(train_body_acc_x)
-trbay <- getMeanList(train_body_acc_y)
-trbaz <- getMeanList(train_body_acc_z)
-
-trbgx <- getMeanList(train_body_gyro_x)
-trbgy <- getMeanList(train_body_gyro_y)
-trbgz <- getMeanList(train_body_gyro_z)
-
-trtax <- getMeanList(train_total_acc_x)
-trtay <- getMeanList(train_total_acc_y)
-trtaz <- getMeanList(train_total_acc_z)
+trainFeatures <- getDataFeatures(xtrain)
+train_data <- data.frame(subject=train_subject, activity=ytrain)
+train_data <- cbind(train_data, trainFeatures)
 
 # read test data
 test_subject <- readLines("dataset/test/subject_test.txt")
 xtest <- readLines("dataset/test/X_test.txt")
 ytest <- readLines("dataset/test/y_test.txt")
 
-test_body_acc_x <- readLines("dataset/test/Inertial Signals/body_acc_x_test.txt")
-test_body_acc_y <- readLines("dataset/test/Inertial Signals/body_acc_y_test.txt")
-test_body_acc_z <- readLines("dataset/test/Inertial Signals/body_acc_z_test.txt")
+testFeatures <- getDataFeatures(xtest)
+test_data <- data.frame(subject=test_subject, activity=ytest)
+test_data <- cbind(test_data, testFeatures)
 
-test_body_gyro_x <- readLines("dataset/test/Inertial Signals/body_gyro_x_test.txt")
-test_body_gyro_y <- readLines("dataset/test/Inertial Signals/body_gyro_y_test.txt")
-test_body_gyro_z <- readLines("dataset/test/Inertial Signals/body_gyro_z_test.txt")
-
-test_total_acc_x <- readLines("dataset/test/Inertial Signals/total_acc_x_test.txt")
-test_total_acc_y <- readLines("dataset/test/Inertial Signals/total_acc_y_test.txt")
-test_total_acc_z <- readLines("dataset/test/Inertial Signals/total_acc_z_test.txt")
-
-tbax <- getMeanList(test_body_acc_x)
-tbay <- getMeanList(test_body_acc_y)
-tbaz <- getMeanList(test_body_acc_z)
-
-tbgx <- getMeanList(test_body_gyro_x)
-tbgy <- getMeanList(test_body_gyro_y)
-tbgz <- getMeanList(test_body_gyro_z)
-
-ttax <- getMeanList(test_total_acc_x)
-ttay <- getMeanList(test_total_acc_y)
-ttaz <- getMeanList(test_total_acc_z)
-
-test_data <- data.frame(subject=test_subject, body_acceleration_x=tbax, body_acceleration_y=tbay, body_acceleration_z=tbaz, body_gyro_x=tbgx, body_gyro_z=tbgz, body_gyro_z=tbgz, total_acceleration_x=ttax, total_acceleration_y=ttay, total_acceleration_z=ttaz, activity=ytest)
-train_data <- data.frame(subject=train_subject, body_acceleration_x=trbax, body_acceleration_y=trbay, body_acceleration_z=trbaz, body_gyro_x=trbgx, body_gyro_z=trbgz, body_gyro_z=trbgz, total_acceleration_x=trtax, total_acceleration_y=trtay, total_acceleration_z=trtaz, activity=ytrain)
+# merge test and train data
 full_data <- rbind(test_data, train_data)
 
-
 # replace activity description
-clean_test_data <- replaceActivityLabels(test_data)
-clean_train_data <- replaceActivityLabels(train_data)
 clean_full_data <- replaceActivityLabels(full_data)
 
-sum_test <- summary(clean_test_data)
-sum_train <- summary(clean_test_data)
-sum_full <- summary(clean_full_data)
-#print(tail(clean_test_data, 5))
-#print(tail(clean_train_data, 5))
-#print(tail(clean_full_data, 5))
-#print(sum_test)
-#print(sum_train)
-#print(sum_full)
+print(head(clean_full_data, 2))
